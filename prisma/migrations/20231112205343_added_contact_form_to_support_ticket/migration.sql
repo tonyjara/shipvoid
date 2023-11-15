d6fce530926e3ff40591fc2b9520a4f67b6a314f
Tony Jara <340studiospy@gmail.com>
2023-11-15 08:58:17 -0300

    new migrations

--- prisma/migrations/20231112205343_added_contact_form_to_support_ticket/migration.sql
+++
@@ -1,2 +0,0 @@
--- AlterEnum
-ALTER TYPE "SupportTicketType" ADD VALUE 'contactForm';
--- prisma/migrations/20231112205451_made_user_not_required_in_support_tickets/migration.sql
+++
@@ -1,8 +0,0 @@
--- DropForeignKey
-ALTER TABLE "SupportTicket" DROP CONSTRAINT "SupportTicket_userId_fkey";
-
--- AlterTable
-ALTER TABLE "SupportTicket" ALTER COLUMN "userId" DROP NOT NULL;
-
--- AddForeignKey
-ALTER TABLE "SupportTicket" ADD CONSTRAINT "SupportTicket_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE SET NULL ON UPDATE CASCADE;
